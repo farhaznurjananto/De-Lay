5ce8e488306371a025448307285365e38eed9153
farhaznurjananto <farhaznurjananto052@gmail.com>
2023-03-30 00:46:31 +0700

    css js

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
 
     <!-- custom css -->
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
     <div class="container m-auto">
